69dfc1d16273f1aa51b72f6b3b4a11a8ed947cae
Olga <64541631+OlgaPoltavets@users.noreply.github.com>
2020-11-23 02:38:39 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -3096,7 +3096,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50887" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65394" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
